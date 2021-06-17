ad263ca42054ff04221482dd4e32a9cdaa995141
idoy <alfinramadhan97@gmail.com>
2021-06-17 16:10:42 +0700

    add basic page for product and navbar

---
+++ pages/ProductCust.jsx
@@ -0,0 +1,12 @@
+
+import Layout from "../components/Layout"
+
+
+export default function Page404() {
+  return (
+    <Layout title="Product Cust">
+      <h1>Halaman Product Cust</h1>
+    </Layout>
+   
+  );
+}
