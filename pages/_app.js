229e6eff878e75000570054637c61ba2693c8466
elazsyahranie <elazaribrahims@gmail.com>
2021-06-18 07:19:54 +0700

    Modified Pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,16 @@
-import Head from "next/head";
 import "../styles/globals.css";
-import "bootstrap/dist/css/bootstrap.css";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from "react-redux";
+import { useStore } from "../redux/store";
 
 function MyApp({ Component, pageProps }) {
+  const store = useStore(pageProps.initialReduxState);
+
   return (
-    <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
+    <Provider store={store}>
       <Component {...pageProps} />
-    </>
+    </Provider>
   );
 }
 
--- redux/reducer/index.js
+++ redux/reducer/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from "redux";
 
-import counter from "./counter";
-import pin from "./pin";
-import user from "./user";
+// import counter from "./counter";
+// import pin from "./pin";
+// import user from "./user";
 
 export default combineReducers({});
