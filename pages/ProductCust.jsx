cfac3ef871601e1c3f0862354b520263df6aae90
idoy <alfinramadhan97@gmail.com>
2021-06-18 10:11:34 +0700

    .

--- pages/ProductCust.jsx
+++ pages/ProductCust.jsx
@@ -1,10 +1,10 @@
-
 import Layout from "../components/Layout"
+import NavBar from  "../components/module/NavBar"
 
-
-export default function Page404() {
+export default function ProductCust() {
   return (
     <Layout title="Product Cust">
+      <NavBar/>
       <h1>Halaman Product Cust</h1>
     </Layout>
    
