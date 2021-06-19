e4ccf63b29f21e72e7306f38dc3b55fdd8ff7333
elazsyahranie <elazaribrahims@gmail.com>
2021-06-19 14:19:33 +0700

    Bullet Point

--- components/module/AdminDashboardNavbar.js
+++ components/module/AdminDashboardNavbar.js
@@ -4,29 +4,35 @@ export default function AdminDasboardNavbar() {
   return (
     <>
       <div className={styles.whiteBackground}>
-        <div className="container py-4">
-          <div className="d-flex justify-content-between py-1">
-            <div className="d-flex">
-              <img src="/coffee 1.png" className="me-2"></img>
-              <span className="fw-bold my-auto ms-2">Coffee Express</span>
+        <div className="container-fluid py-4" style={{ maxWidth: "1116px" }}>
+          <div className="row h-100 w-100">
+            <div className="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+              <div className="d-flex h-100">
+                <img src="/coffee 1.png" className="me-2 my-auto"></img>
+                <span className="fw-bold my-auto ms-2">Coffee Express</span>
+              </div>
             </div>
-            <div className="d-flex my-auto">
-              <span className="my-auto mx-4">Home</span>
-              <span className="my-auto mx-4">Product</span>
-              <span className="my-auto mx-4">Orders</span>
-              <span className="my-auto mx-4">Dashboard</span>
+            <div className="col-lg-4 col-md-4 col-sm-4 col-xs-12 text-center my-auto">
+              <div className="d-flex justify-content-around flex-wrap">
+                <span className="my-auto mx-1">Home</span>
+                <span className="my-auto mx-1">Product</span>
+                <span className="my-auto mx-1">Orders</span>
+                <span className="my-auto mx-1">Dashboard</span>
+              </div>
             </div>
-            <div className="d-flex">
-              <img src="/Vector.png" style={{ height: "30px" }}></img>
-              <img
-                src="/chat (1) 1.png"
-                style={{ height: "30px" }}
-                className="mx-5"
-              ></img>
-              <img
-                src="/image 39.png"
-                style={{ height: "30px", borderRadius: "50%" }}
-              ></img>
+            <div className="col-lg-4 col-md-4 col-sm-4 col-xs-12 my-auto">
+              <div className="d-flex justify-content-end flex-wrap">
+                <img src="/Vector.png" style={{ height: "30px" }}></img>
+                <img
+                  src="/chat (1) 1.png"
+                  className="mx-3"
+                  style={{ height: "30px" }}
+                ></img>
+                <img
+                  src="/image 39.png"
+                  style={{ height: "30px", borderRadius: "50%" }}
+                ></img>
+              </div>
             </div>
           </div>
         </div>
--- pages/profile/AdminDashboard/index.js
+++ pages/profile/AdminDashboard/index.js
@@ -8,7 +8,40 @@ export default function AdminDashboard() {
       <Layout title="Admin Dashboard">
         <div className={styles.greyBackground}>
           <AdminDashboardNavbar />
-          <h5>Admin Dashboard</h5>
+          <div className="container">
+            <h5 className={`fw-bold text-center pt-4 pb-3 ${styles.brownText}`}>
+              See how your store progressed so far
+            </h5>
+            <div className="d-flex justify-content-center flex-wrap pb-3">
+              <div className="row mx-1">
+                <div className="d-flex justify-content-center">
+                  <img
+                    src="/Ellipse 189.png"
+                    className={styles.bulletPeriod}
+                  ></img>
+                </div>
+                <span className="text-center">Daily</span>
+              </div>
+              <div className="row mx-1">
+                <div className="d-flex justify-content-center">
+                  <img
+                    src="/Ellipse 189.png"
+                    className={styles.bulletPeriod}
+                  ></img>
+                </div>
+                <span className="text-center">Weekly</span>
+              </div>
+              <div className="row mx-1">
+                <div className="d-flex justify-content-center">
+                  <img
+                    src="/Ellipse 189.png"
+                    className={styles.bulletPeriod}
+                  ></img>
+                </div>
+                <span className="text-center">Monthly</span>
+              </div>
+            </div>
+          </div>
         </div>
       </Layout>
     </>
