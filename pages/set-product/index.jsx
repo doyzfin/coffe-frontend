bf288bea052f219ef6c3acbea77b453cc077ee76
idoy <alfinramadhan97@gmail.com>
2021-06-20 21:27:12 +0700

    after pull

--- pages/set-product/index.jsx
+++ pages/set-product/index.jsx
@@ -3,11 +3,21 @@ import NavBar from "components/module/NavBar";
 import Footer from "components/module/footer";
 import { Container, Row, Col, Card, Button, Form } from "react-bootstrap";
 import styles from "../../styles/NewProduct.module.css";
-import { useState } from "react";
+import React, { useState } from "react";
 
 export default function newProduct() {
   const [isClickSize, setIsClickSize] = useState(false);
   const [isClickCoffee, setIsClickCoffee] = useState(false);
+  const [isImage, setIsImage] = useState(false);
+  const [imageProduct, setImageProduct] = useState("");
+  const [formProduct, setFormProduct] = useState({
+    productName: "",
+    productPrice: "",
+    productCategory: "",
+    productSize: "",
+    productDesc: "",
+    image: null,
+  });
 
   const handleClickSizeCoffee = () => {
     setIsClickCoffee(true);
@@ -18,7 +28,31 @@ export default function newProduct() {
     setIsClickSize(true);
     setIsClickCoffee(false);
   };
-
+  const inputOpenFileRef = React.createRef();
+  const showOpenFileDlg = () => {
+    inputOpenFileRef.current.click();
+  };
+  const handleImage = (event) => {
+    event.preventDefault();
+    setIsImage(true);
+    setImageProduct(URL.createObjectURL(event.target.files[0]));
+    setFormProduct({ ...formProduct, image: event.target.files[0] });
+  };
+  const changeText = (event) => {
+    setFormProduct({ ...formProduct, [event.target.name]: event.target.value });
+  };
+  const handleCancel = () => {
+    setIsImage(false);
+    setFormProduct({
+      productName: "",
+      productPrice: "",
+      productCategory: "",
+      productSize: "",
+      productDesc: "",
+      image: null,
+    });
+  };
+  console.log(formProduct);
   return (
     <Layout title="Add Product">
       <NavBar />
@@ -28,21 +62,38 @@ export default function newProduct() {
         </p>
         <Row>
           <Col sm={5}>
-            <Card className={styles.forImg}>
-              <img alt="" src="/camera.png" className={styles.camera} />
+            <input
+              ref={inputOpenFileRef}
+              type="file"
+              style={{ display: "none" }}
+              onChange={(event) => handleImage(event)}
+            />
+            <Card className={isImage ? styles.cardImgProduct : styles.forImg}>
+              <img
+                alt=""
+                src={isImage ? imageProduct : "/camera.png"}
+                className={isImage ? styles.imageProduct : styles.camera}
+              />
             </Card>
-            <Button className={styles.btnGalery}>Choose from gallery</Button>
+            <Button className={styles.btnGalery} onClick={showOpenFileDlg}>
+              Choose from gallery
+            </Button>
             <Button className={styles.btnSave}>Save Product</Button>
-            <Button className={styles.btnCancel}>Cancel</Button>
+            <Button className={styles.btnCancel} onClick={handleCancel}>
+              Cancel
+            </Button>
           </Col>
           <Col sm={7} className={styles.formInput}>
             <Form>
-              <Form.Group controlId="formEmail">
+              <Form.Group controlId="formText">
                 <Form.Label className={styles.nameLabel}>Name :</Form.Label>
                 <Form.Control
-                  type="email"
+                  type="text"
                   placeholder="Type product name min. 50 characters"
                   className={styles.controlInput}
+                  name="productName"
+                  value={formProduct.productName}
+                  onChange={(event) => changeText(event)}
                 />
               </Form.Group>
               <Row>
@@ -55,6 +106,9 @@ export default function newProduct() {
                       type="text"
                       placeholder="Type the price"
                       className={styles.controlInput}
+                      name="productPrice"
+                      value={formProduct.productPrice}
+                      onChange={(event) => changeText(event)}
                     />
                   </Form.Group>
                 </Col>
@@ -63,7 +117,13 @@ export default function newProduct() {
                     <Form.Label className={styles.nameLabel}>
                       Category :
                     </Form.Label>
-                    <Form.Control as="select" className={styles.controlInput}>
+                    <Form.Control
+                      as="select"
+                      className={styles.controlInput}
+                      name="productCategory"
+                      value={formProduct.productCategory}
+                      onChange={(event) => changeText(event)}
+                    >
                       <option value="">Select category</option>
                       <option value="Favorite Product">Favorite Product</option>
                       <option value="Coffee">Coffee</option>
@@ -82,6 +142,9 @@ export default function newProduct() {
                   type="text"
                   placeholder="Describe your product min. 150 characters"
                   className={styles.controlInput}
+                  name="productDesc"
+                  value={formProduct.productDesc}
+                  onChange={(event) => changeText(event)}
                 />
               </Form.Group>
               <Form.Group controlId="formEmail">
