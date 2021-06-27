a0a78a1373659e1e0c20a8a252fc5dc10ffaacf8
Timotius-Nugroho <timotiusnugroho999@gmail.com>
2021-06-27 17:08:37 +0700

    smo shit ajustment

--- components/module/HomeNavbar.js
+++ components/module/HomeNavbar.js
@@ -1,106 +1,139 @@
-import { Container, Navbar, Nav, Row, Col } from "react-bootstrap";
+import { Container, Navbar, Nav, Form, Row, Col } from "react-bootstrap";
 import styles from "../../styles/NavBar.module.css";
-import homeStyles from "../../styles/HomeNavbar.module.css";
 import { useState, useEffect } from "react";
-import Link from "next/link";
 import { useRouter } from "next/router";
 import Cookie from "js-cookie";
 
 import { connect } from "react-redux";
+import { setKeywords } from "redux/actions/keywords";
 import { getUser } from "redux/actions/user";
 
-function HomeNavBar(props) {
+function NavBar(props) {
   const router = useRouter();
+  const [search, setSearch] = useState("");
   const [user, setUser] = useState({});
 
   useEffect(() => {
-    if (Cookie.get("token")) {
-      props
-        .getUser(Cookie.get("userId"), Cookie.get("token"))
-        .then((res) => {
-          setUser(res.value.data.data[0]);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }
+    props
+      .getUser(Cookie.get("userId"), Cookie.get("token"))
+      .then((res) => {
+        if (!props.global) {
+          if (res.value.data.data[0].user_role !== "customer") {
+            window.location.href = "/product-admin";
+          }
+        }
+        setUser(res.value.data.data[0]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }, []);
+
+  const setKeywordsStore = (text) => {
+    setSearch(text);
+    props.setKeywords(text);
+    if (props.catchKey) {
+      props.catchKey(text);
+    }
+  };
 
   const moveToProfile = () => {
     router.push("/profile");
   };
 
-  const moveToSignup = () => {
-    router.push("/signup");
-  };
-
-  // console.log(user);
+  // console.log("NAV", user);
   return (
     <Container fluid className={styles.main}>
-      <Navbar expand="lg" className={styles.navbarWidth}>
-        <Navbar.Brand href="#" className={styles.brand}>
-          <Row>
-            <Col>
-              <img alt="" src="/coffee 1.png" />
-            </Col>
-            <Col className={styles.colBrand}>Coffee Express</Col>
-          </Row>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className={styles.form}>
-            <Nav.Link
-              className={styles.navLink}
-              onClick={() => {
-                router.push("/");
-              }}
-            >
-              Home
-            </Nav.Link>
-            <Nav.Link
-              className={styles.navLink}
-              onClick={() => {
-                router.push("/product-admin");
-              }}
-            >
-              Product
-            </Nav.Link>
-            <Nav.Link
-              className={styles.navLink}
-              onClick={() => {
-                router.push("/manage-order-admin");
-              }}
-            >
-              Orders
-            </Nav.Link>
-            <Nav.Link
-              className={styles.navLink}
-              onClick={() => {
-                router.push("/admin-dashboard");
-              }}
-            >
-              Dashboard
-            </Nav.Link>
-          </Nav>
-          <Nav.Link
-            onClick={() => {
-              router.push("/login");
-            }}
-            className={`${styles.navLink} me-5 fw-bold`}
-          >
-            Login
-          </Nav.Link>
-          <button
-            onClick={moveToSignup}
-            className={`${homeStyles.yellowButton} fw-bold`}
-          >
-            Sign Up
-          </button>
-        </Navbar.Collapse>
-      </Navbar>
+      <Container>
+        <Navbar expand="lg">
+          <Navbar.Brand href="#" className={styles.brand}>
+            <Row>
+              <Col>
+                <img alt="" src="/coffee 1.png" />
+              </Col>
+              <Col className={styles.colBrand}>Coffee Express</Col>
+            </Row>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className={styles.form}>
+              <Nav.Link
+                className={styles.navLink}
+                onClick={() => {
+                  router.push("/");
+                }}
+              >
+                Home
+              </Nav.Link>
+              <Nav.Link
+                className={styles.navLink}
+                onClick={() => {
+                  router.push("/product-cust");
+                }}
+              >
+                Product
+              </Nav.Link>
+              <Nav.Link
+                href="#"
+                className={styles.navLink}
+                onClick={() => {
+                  router.push("/payment");
+                }}
+              >
+                Your Chart
+              </Nav.Link>
+              <Nav.Link
+                href="#"
+                className={styles.navLink}
+                onClick={() => {
+                  router.push("/history-cust");
+                }}
+              >
+                History
+              </Nav.Link>
+            </Nav>
+            {props.catchKey ? (
+              <Form>
+                <Form.Control
+                  type="text"
+                  placeholder="Search"
+                  className={styles.formControl}
+                  onChange={(event) => {
+                    setKeywordsStore(event.target.value);
+                  }}
+                />
+              </Form>
+            ) : (
+              ""
+            )}
+            <img alt="" src="/chat.png" className={styles.notif} />
+            {user.user_image ? (
+              <img
+                alt=""
+                src={`${process.env.IMAGE_URL}/${user.user_image}`}
+                className={styles.profile}
+                style={{ cursor: "pointer" }}
+                onClick={() => {
+                  moveToProfile();
+                }}
+              />
+            ) : (
+              <img
+                alt=""
+                src="/no-img.png"
+                className={styles.profile}
+                style={{ cursor: "pointer" }}
+                onClick={() => {
+                  moveToProfile();
+                }}
+              />
+            )}
+          </Navbar.Collapse>
+        </Navbar>
+      </Container>
     </Container>
   );
 }
 
-const mapDispatchToProps = { getUser };
-export default connect(null, mapDispatchToProps)(HomeNavBar);
+const mapDispatchToProps = { setKeywords, getUser };
+export default connect(null, mapDispatchToProps)(NavBar);
